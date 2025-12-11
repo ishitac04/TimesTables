06163872b0fe2bc0955e594061ad41103544ba38
ishitac04 <ishitaa.chowdhury@gmail.com>
2025-12-11 23:00:47 +0000

    Made the times table logic part of the grid, made it detect typing, added a score box

--- code.js
+++ code.js
@@ -1,18 +1,44 @@
 let x;
 let y;
-let ans;
+let ans="";
 let correctans;
+let score=0;
+let questioncount=0;
+
+let innerinnerbox = document.getElementById("innerinnerbox");
+let scorebox = document.getElementById("scorebox");
 
 function generateQuestion() {
-    x = Math.floor(Math.random()*20);
-    y = Math.floor(Math.random()*12);
-    ans=prompt(x+" x "+y+" = ")
+    ans="";
+    questioncount=questioncount+1;
+    x = Math.floor(Math.random()*19) + 2;
+    y = Math.floor(Math.random()*11) + 2;
+    innerinnerbox.textContent = x+" x "+y+" = ";
     correctans=x*y;
-    if (ans == correctans) {
-        alert("correct!")
-    } else {
-        alert("The answer should be "+correctans+". Your answer was "+ans+".")
-    }
 }
 
-generateQuestion();
+document.addEventListener("keydown", (e) => {
+    if (e.key >= "0" && e.key <= "9") {
+        ans += e.key;
+        innerinnerbox.textContent = x+" x "+y+" = "+ans;
+    }
+
+    if (e.key == "Backspace") {
+        ans = ans.slice(0, -1);
+        innerinnerbox.textContent = x+" x "+y+" = "+ans;
+    }
+
+    if (e.key == "Enter") {
+        if (Number(ans) == correctans) {
+            score++;
+            scorebox.textContent = "Score: "+score;
+        } else {
+            alert("The answer was "+correctans);
+        }
+        generateQuestion();
+    }
+});
+
+for (i=0;i<30;i++) {
+    generateQuestion();
+}
